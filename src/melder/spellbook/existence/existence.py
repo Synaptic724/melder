85e570d70ed22768e8b27bb0aa689d7a326d440e
Mark <MtG@live.ca>
2025-04-24 19:29:45 -0600

    Refactor file structure and enhance existence states for conduit management

--- src/melder/spellbook/existence/existence.py
+++ src/melder/spellbook/existence/existence.py
@@ -7,6 +7,8 @@ class Existence(Enum):
     unique = auto()               # Single instance across all of Aether
     unique_per_conduit = auto()    # One instance per conduit
     many = auto()                  # New instance every time
+    unique_per_conduit_cluster = auto()  # One instance per conduit cluster, users can cluster conduits into their own desired groups owned by the aether
+    unique_per_conduit_lineage = auto()  # One instance per conduit lineage, a lineage is a tree of conduits from child to parent
 #    dynamic = auto()               # Instance that can mutate/upgrade at runtime   // Not really sure what usecase we got here for this
 
     def __str__(self):
--- tests/unittests/melder/utilities/test_directed_acyclic_work_graph.py
+++ tests/unittests/melder/utilities/test_directed_acyclic_work_graph.py
@@ -1,6 +1,6 @@
 import unittest
 import time
-from melder.aether.conduit.meld.dependency_crawler.dag.directed_acyclic_work_graph import Node, Edge, DirectedAcyclicWorkGraph, StateObject, ExecutionContext
+from melder.spellbook.bind.graph_builder.dag import Node, Edge, DirectedAcyclicWorkGraph, StateObject, ExecutionContext
 
 # Assuming you have all your DAG code in a module named 'dag_module'
 # For example:
