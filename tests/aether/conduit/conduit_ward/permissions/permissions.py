79b3994ac04e047b22b73e9b22239a36ea9e37d7
Mark <MtG@live.ca>
2025-11-08 15:35:49 -0700

    Remove unused classes and enums related to permissions, conduit states, and spell crafting for improved code clarity and maintainability

--- tests/aether/conduit/conduit_ward/permissions/permissions.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum, auto
-
-class Permissions(Enum):
-    """
-    Each level of permission inherits from the previous one.
-    """
-    read = auto() # Allows reading data
-    create = auto() # Allows creating new data, and includes read
-    block = auto() # Allows blocking data, used only within Spellbook
