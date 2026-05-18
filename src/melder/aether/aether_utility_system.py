3367c029be71b7c8361d9cd9db1c42b658213c61
Mark <MtG@live.ca>
2026-05-18 13:24:18 -0600

    fix: refine ACL error message

--- src/melder/aether/aether_utility_system.py
+++ src/melder/aether/aether_utility_system.py
@@ -1,7 +1,6 @@
 import logging
 import threading
 from typing import Any, Callable, Dict, Iterable, Optional, Union
-
 from melder.__melder_registration_guard__ import __melder_registration_guard__ as _mrg
 from melder.utilities.general_base.cleanable import Cleanable
 from melder.utilities.interfaces.ichannellogger import IChannelLogger
